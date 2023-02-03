f645ac8bb509230d24edece1e09ec9abaaa213d9
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-04 01:00:06 +0200

    Move to Marker On Click

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 class Workout {
     date = new Date();
     id = (Date.now() + "").slice(-10);
+    clicks = 0;
 
     constructor(coords, distance, duration) {
         this.coords = coords; // [lat, lng]
@@ -29,6 +30,10 @@ class Workout {
         this.description = `${this.type[0].toUpperCase()}${this.type.slice(
             1
         )} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+    }
+
+    click() {
+        this.clicks++;
     }
 }
 
@@ -81,12 +86,17 @@ const inputElevation = document.querySelector(".form__input--elevation");
 
 class App {
     #map;
+    #mapZoomLevel = 13;
     #mapEvent;
     #workouts = [];
     constructor() {
         this._getPosition();
         form.addEventListener("submit", this._newWorkout.bind(this));
         inputType.addEventListener("change", this._toggleElevationField);
+        containerWorkouts.addEventListener(
+            "click",
+            this._moveToPopup.bind(this)
+        );
     }
 
     _getPosition() {
@@ -107,7 +117,7 @@ class App {
 
         const coords = [latitude, longitude];
 
-        this.#map = L.map("map").setView(coords, 13);
+        this.#map = L.map("map").setView(coords, this.#mapZoomLevel);
 
         L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
             attribution:
@@ -282,6 +292,30 @@ class App {
             `;
         }
         form.insertAdjacentHTML("afterend", html);
+    }
+
+    _moveToPopup(e) {
+        const workoutEl = e.target.closest(".workout");
+
+        if (!workoutEl) return;
+
+        console.log(workoutEl);
+
+        const workout = this.#workouts.find(
+            (work) => work.id === workoutEl.dataset.id
+        );
+
+        console.log(workout);
+
+        this.#map.setView(workout.coords, this.mapZoomLevel, {
+            animate: true,
+            pan: {
+                duration: 1,
+            },
+        });
+
+        // using the public interface
+        workout.click();
     }
 }
 
